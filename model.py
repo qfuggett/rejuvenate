122745797a71e26efa924d276333ec1c24458697
qfuggett <qfuggett@gmail.com>
2021-04-06 15:13:07 +0000

    create connect_to_db function and module check

--- model.py
+++ model.py
@@ -139,3 +139,24 @@ class CleanseLog(db.Model):
     def __repr__(self):
         return f'<CleanseLog cleanse_log_id={self.cleanse_log_id} user_cleanse_id={self.user_cleanse_id} timestamp={self.timestamp} comment={self.comment} private={self.private}>'
 
+
+
+def connect_to_db(flask_app, db_uri='postgresql:///rejuvenate', echo=True):
+    flask_app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    flask_app.config['SQLALCHEMY_ECHO'] = echo
+    flask_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.app = flask_app
+    db.init_app(flask_app)
+
+    print('Connected to the db!')
+
+
+if __name__ == '__main__':
+    from server import app
+
+    # Call connect_to_db(app, echo=False) if your program output gets
+    # too annoying; this will tell SQLAlchemy not to print out every
+    # query it executes.
+
+    connect_to_db(app)
